a7b7330045c9a2e6d88507efb39de550543ebfa5
Wojtek Książek <wojtek99955@gmail.com>
2022-04-13 22:52:30 +0200

    index content update

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
 import styled from "styled-components";
 
-const MainTitle = styled.h1`
-  font-size: 3.2rem;
+const Main = styled.main`
   position: absolute;
   top: 40%;
   text-align: center;
   left: 0;
   right: 0;
+  width: 60%;
+  margin: auto;
+
+  h1 {
+    font-size: 3.2rem;
+  }
+  p {
+    font-size: 1.3rem;
+    line-height: 2rem;
+  }
 `;
 export default function Home() {
   return (
     <>
-      <MainTitle>Welcome to my NextJS website!</MainTitle>
+      <Main>
+        <h1>Welcome to my NextJS website!</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Reprehenderit perferendis praesentium voluptatum aspernatur numquam.
+          Placeat officia deleniti laboriosam labore exercitationem, quisquam
+          nihil sunt est culpa ratione iste mollitia eveniet nam doloribus eum
+          eaque reprehenderit totam tenetur nemo quam ab neque nostrum hic! Nemo
+          deserunt in repellendus quisquam amet. Odit, quod!
+        </p>
+      </Main>
     </>
   );
 }
