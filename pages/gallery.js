da1f780db44407835d5163ce5823b7aebc0eeb25
Wojtek Książek <wojtek99955@gmail.com>
2022-04-14 14:58:52 +0200

    Gallery section added

---
+++ pages/gallery.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Section = styled.section``;
+
+function gallery() {
+  return (
+    <>
+      <div>gallery</div>
+    </>
+  );
+}
+
+export default gallery;
