d21aae2828475c0ac0d27ef5ba690464126c3239
Wojtek Książek <wojtek99955@gmail.com>
2022-04-14 17:06:18 +0200

    Gallery Image component added

---
+++ assets/Images.js
@@ -0,0 +1,30 @@
+export const images = [
+  {
+    id: "img-1",
+    src: "/images/1.jpg",
+  },
+  {
+    id: "img-2",
+    src: "/images/2.jpg",
+  },
+  {
+    id: "img-3",
+    src: "/images/3.jpg",
+  },
+  {
+    id: "img-4",
+    src: "/images/4.jpg",
+  },
+  {
+    id: "img-5",
+    src: "/images/5.jpg",
+  },
+  {
+    id: "img-6",
+    src: "/images/6.jpg",
+  },
+  {
+    id: "img-7",
+    src: "/images/7.jpg",
+  },
+];
--- pages/gallery.js
+++ pages/gallery.js
@@ -1,12 +1,36 @@
+import Image from "next/image";
 import styled from "styled-components";
+import { images } from "../assets/Images";
 
-const Section = styled.section``;
+const Section = styled.section`
+  h1 {
+    font-size: 2.5rem;
+    text-align: center;
+  }
+  div {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 3rem;
+  }
+`;
 
 function gallery() {
   return (
-    <>
-      <div>gallery</div>
-    </>
+    <Section>
+      <h1>Gallery</h1>
+      {images.map((image) => {
+        return (
+          <div key={image.id}>
+            <Image
+              alt={image.id}
+              src={`${image.src}`}
+              width="280"
+              height="420"
+            />
+          </div>
+        );
+      })}
+    </Section>
   );
 }
 
