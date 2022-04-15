d5b56df0ba224ceff9007b908c579d8c116bbb48
Wojtek Książek <wojtek99955@gmail.com>
2022-04-15 19:08:20 +0200

    Users fetch data with getstaticprops

--- pages/users/index.js
+++ pages/users/index.js
@@ -7,10 +7,22 @@ const Container = styled.div`
   }
 `;
 
-function index() {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { users: data },
+  };
+};
+
+function index({ users }) {
   return (
     <Container>
       <h1>Users</h1>
+      {users.map((user) => {
+        return <p key={user.id}>{user.name}</p>;
+      })}
     </Container>
   );
 }
