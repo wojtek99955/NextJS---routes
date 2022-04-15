6abdffbb57b132f34f138e058e78f3e19f274e06
Wojtek Książek <wojtek99955@gmail.com>
2022-04-15 19:37:23 +0200

    Users styles update

--- pages/users/index.js
+++ pages/users/index.js
@@ -7,6 +7,15 @@ const Container = styled.div`
   }
 `;
 
+const UsersWrapper = styled.div`
+  max-width: 500px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem 0;
+`;
+
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
@@ -16,13 +25,44 @@ export const getStaticProps = async () => {
   };
 };
 
+const UserContainer = styled.div`
+  border: 1px solid #d8d4dc;
+  padding: 1rem;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  flex-grow: 0;
+  align-items: center;
+
+  button {
+    border: none;
+    background-color: #5099f4;
+    color: white;
+    height: 2.5rem;
+    font-size: 1.2rem;
+    border-radius: 5%;
+    padding: 0 1rem;
+    cursor: pointer;
+    &:hover {
+      background-color: #2871cc;
+    }
+  }
+`;
+
 function index({ users }) {
   return (
     <Container>
-      <h1>Users</h1>
-      {users.map((user) => {
-        return <p key={user.id}>{user.name}</p>;
-      })}
+      <UsersWrapper>
+        <h1>Users</h1>
+        {users.map((user) => {
+          return (
+            <UserContainer key={user.id}>
+              <h3>{user.name}</h3>
+              <button>More</button>
+            </UserContainer>
+          );
+        })}
+      </UsersWrapper>
     </Container>
   );
 }
