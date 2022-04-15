bb0bade8cba5d8d0387ebc2f1851a879785da3e9
Wojtek Książek <wojtek99955@gmail.com>
2022-04-15 20:11:53 +0200

    User details page fetch

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -2,10 +2,38 @@ import styled from "styled-components";
 
 const Container = styled.div``;
 
-function Details() {
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  const paths = data.map((user) => {
+    return {
+      params: { id: user.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { user: data },
+  };
+};
+
+function Details({ user }) {
   return (
     <Container>
       <h1>User Details</h1>
+      <h3>{user.name}</h3>
+      <p>{user.email}</p>
+      <p>{user.phone}</p>
     </Container>
   );
 }
